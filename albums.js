f7d079856ae9d71f74114e1ddb5c84e07c5e9748
Jack Cooper-McEwan <jackcooper93@gmail.com>
2023-05-31 11:51:54 +0100

    looks pretty commit

--- albums.js
+++ albums.js
@@ -1,7 +1,8 @@
 function parseData(data) {
-    d = data['history'].filter(d => Number.isInteger(d.rating))
-console.log(d)
-    
+    d = data['history']
+    .filter(d => Number.isInteger(d.rating))
+    .sort((a, b) => d3.descending(a.rating, b.rating))
+
     var t = d3.select("#holder")
     .selectAll("div")
     .data(d)
@@ -15,6 +16,11 @@ console.log(d)
     t
     .append("img")
     .attr("src", d => d.album.images[1]["url"])
+
+    var d = t.append("div")
+    .attr("class", "img-text")
+    .text(d => "★".repeat(Number(d.rating)).padEnd(5, "☆"))
 }
 
-d3.json("sample.json", parseData);
+//d3.json("sample.json", parseData);
+d3.json("https://1001albumsgenerator.com/api/v1/projects/cqkh42", parseData);
